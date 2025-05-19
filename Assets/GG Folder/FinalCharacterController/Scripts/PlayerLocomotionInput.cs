f0ecfd5472298e86ecaaf16264097ee43a2ca41f
amirandagoeller6623 <amirandagoeller6623@student.pps.net>
2025-05-19 11:04:01 -0700

    initial commit

---
+++ Assets/GG Folder/FinalCharacterController/Scripts/PlayerLocomotionInput.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GinjaGaming.FinalCharacterController
+{
+    public class PlayerLocomotionInput : MonoBehavior, PlayerControls.IPlayerLocomotionMapActions
+    {
+        public PlayerControls PlayerControls { get; private set; }
+        public Vector2 MovementInput {get; private set; }
+        
+        
+        private void OnEnable()
+        {
+            PlayerControls = new PlayerControls();
+            PlayerControls.Enable();
+            
+            PlayerControls.PlayerLocomotionMap.Enable();
+            PlayerControls.PlayerLocomotionMap.SetCallbacks(this);
+        }
+        
+        private void OnDisable()
+        {
+            PlayerControls.PlayerLocomotionMap.Disable();
+            PlayerControls.PlayerLocomotionMap.RemoveCallbacks(this);
+        }
+        public void OnMovement(InputAction.CallbackContext context)
+        {
+            MovementInput = context.ReadValue<Vector2>();
+            print(MovementInput);
+        }
+    }
+}
